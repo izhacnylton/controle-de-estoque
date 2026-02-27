a0380bae0549e6f0ed5ca0eefcae26319b3472de
Izhac Nylton <izhac2006@gmail.com>
2026-02-27 16:56:12 -0400

    conexao com o banco de dados

---
+++ src/banco.php
@@ -0,0 +1,23 @@
+<?php 
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "estoque";
+
+//conexao com o banco de dados
+
+try {
+    //tentativa de conexao com o banco de dados
+    //classe PDO  é usada para acessar varios tipos de bancos de dados usando a mesma função
+    /*DSN DATA SOURCE NAME é a string de conexão usada para especificar o tipo de banco de
+    dados ... o host e o nome do banco de dados*/
+
+    $conexao = new PDO("
+    mysql:host=$servidor;
+    dbname=$banco;
+    charset=utf8",
+    $usuario,
+    $senha);
+} catch (\Throwable $erro) {
+    //lançavel serve para qualquer tipo de erro ou exceçao
+}
